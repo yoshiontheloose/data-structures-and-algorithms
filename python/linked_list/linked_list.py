f174d78e75396179ac848214bbfd4dddc5d620c1
Clarissa P <clarissapamonicutt@gmail.com>
2021-12-18 18:10:02 -0800

    initial commit, empty node tests passing

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,25 @@
+
+class Node:
+    def __init__(self,value, next=None):
+        self.value = value
+        self.next=next
+
+
 class LinkedList:
     """
-    Put docstring here
+    Create a Linked List class, include a head property
     """
 
-    def __init__(self):
+    def __init__(self, head):
         # initialization here
+        self.head = head
         pass
 
-    def some_method(self):
-        # method body here
+    def insert(self):
+        pass
+
+    def includes(self):
+        pass
+
+    def to_string(self):
         pass

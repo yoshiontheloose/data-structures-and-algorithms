fe70720a47cc0f2c0aa4bf0594a16ab28fc7b961
Clarissa P <clarissapamonicutt@gmail.com>
2022-01-29 23:07:32 -0800

    initial commit, unfinished, working on whiteboard

---
+++ python/code_challenges/trees/trees.py
@@ -0,0 +1,34 @@
+
+
+
+
+class Node(self, value, left, right):
+    self.value = value
+    self.left = left
+    self.right = right
+
+
+
+class BinaryTree(self):
+
+    def pre_order(self):
+
+
+
+    def in_order(self):
+
+
+
+    def post_order(self):
+
+
+
+
+
+class BinarySearchTree(self):
+
+    def add(self):
+
+
+
+    def contains(self):

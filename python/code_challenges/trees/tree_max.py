fc9e25e61010f923ce64aabc180eed6963626fff
Clarissa P <clarissapamonicutt@gmail.com>
2022-01-31 23:57:26 -0800

    initial commit

---
+++ python/code_challenges/trees/tree_max.py
@@ -0,0 +1,15 @@
+import pytest
+from trees import BinaryTree
+
+
+# check the root value
+# if there is no value, return the max.
+# if root is greater than max, assign max as root value
+# check left node with max
+# check right node with max
+# assign the node with greater value as max
+# return max
+
+
+def tree_max(self):
+
